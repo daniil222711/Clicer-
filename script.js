5a33fb9cb142c23cac1e1403223de90571c4303c
daniil222711 <daniil222711@gmail.com>
2025-05-18 22:10:44 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let count = 0;
-const countElem = document.getElementById('count');
-const button = document.getElementById('clicker-button');
-
-button.addEventListener('click', () => {
-  count += 1; // увеличиваем счётчик на 1
-  countElem.textContent = count + ' рублей';
-});
